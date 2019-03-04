97f4d0a1e3e1678639068a7f88304e03f0f03e66
Iyahiya Hussan Imon <32937176+iyahiyaimon@users.noreply.github.com>
2019-03-03 22:05:15 -0800

    Add files via upload

---
+++ assets/js/js.js
@@ -0,0 +1,68 @@
+
+/**********************************************************
+		BEGIN: PARENT TESTIMONIAL && TEACHER
+**********************************************************/
+jQuery(document).ready(function($){
+	
+	"use strict";
+	
+	if(jQuery().owlCarousel){
+		//Parent testimonial
+		jQuery("#parentTestimonial").owlCarousel({
+			loop:true,
+			margin:10,
+			responsiveClass:true,
+			responsive:{
+				0:{
+					items:1,
+					nav:true,
+					loop: true
+				},
+				991:{
+					items:1,
+					nav:true,
+					loop: true
+				},
+				1170:{
+					items:2,
+					nav:true,
+					loop:true
+				}
+			}		
+		});
+		
+		//Teacher 
+		jQuery("#ourTeacher").owlCarousel({
+			loop:true,
+			margin:10,
+			responsiveClass:true,
+			responsive:{
+				0:{
+					items:1,
+					nav:true,
+					loop: true
+				},
+				991:{
+					items:2,
+					nav:true,
+					loop: true
+				},
+				1170:{
+					items:3,
+					nav:true,
+					loop:true
+				}
+			}		
+		});
+	}
+	//Datepicker
+	jQuery("#studentDOB").datepicker();
+	jQuery("#dptDate").datepicker();
+	
+	//accordion
+	jQuery( "#pAccordion" ).accordion({
+		collapsible:true,
+		icons: { "header": "fa fa-plus", "activeHeader": "fa fa-minus" },
+		heightStyle: "content"
+	});
+});
